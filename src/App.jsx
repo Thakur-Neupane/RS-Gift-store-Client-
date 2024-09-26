fc1b7961bbb96ed79e98209470c5fa6185b2d219
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-09-26 10:50:30 +1000

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+// Pages
 import Login from "./pages/user/Login";
 import Register from "./pages/user/Register";
 import UserVerification from "./pages/user/UserVerification";
-import { AdminLayout } from "./components/layout/AdminLayout";
+import ForgetPassword from "./pages/user/ForgetPassword";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Categories from "./pages/category/Categories";
 import Products from "./pages/product/Products";
-import { Profile } from "./pages/user/Profile";
-import User from "./pages/user/User";
-import Reviews from "./pages/review/Reviews";
-import Admin from "./pages/user/Admin";
-import Orders from "./pages/order/Orders";
-import { Auth } from "./components/auth/Auth";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { autoLoginAction } from "./features/users/userAction";
-import ForgetPassword from "./pages/user/ForgetPassword";
-import NewProduct from "./pages/product/NewProduct";
-import SubCategoryPage from "./pages/sub-category/SubCategory";
-import ProductsDashboard from "./pages/Dashboard/ProductDashboard";
-import EditProduct from "./components/forms/EditProduct";
 import ProductView from "./pages/product/ProductView";
+import NewProduct from "./pages/product/NewProduct";
+import EditProduct from "./components/forms/EditProduct";
+import ProductsDashboard from "./pages/Dashboard/ProductDashboard";
+import Orders from "./pages/order/Orders";
+import Reviews from "./pages/review/Reviews";
+import User from "./pages/user/User";
+import Profile from "./pages/user/Profile";
+import Admin from "./pages/user/Admin";
 import UserDetails from "./pages/user/UserAction";
+import SubCategoryPage from "./pages/sub-category/SubCategory";
 import Coupon from "./pages/coupon/Coupon";
+
+// Layouts
+import { AdminLayout } from "./components/layout/AdminLayout";
+
+// Auth
+import { Auth } from "./components/auth/Auth";
+import { autoLoginAction } from "./features/users/userAction";
 
 function App() {
   const dispatch = useDispatch();
@@ -36,43 +42,43 @@ function App() {
   return (
     <div>
       <Routes>
-        {/* Public routes */}
+        {/* Public Routes */}
         <Route path="/" element={<Login />} />
         <Route path="/verify-user" element={<UserVerification />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="admin/new" element={<Register />} />
         <Route path="/users/:id" element={<UserDetails />} />
 
-        {/* Private routes */}
+        {/* Private Routes */}
         <Route
-          path="/"
+          path="/admin/*"
           element={
             <Auth>
               <AdminLayout />
             </Auth>
           }
         >
-          <Route path="admin/dashboard" element={<Dashboard />} />
-          <Route path="admin/categories" element={<Categories />} />
-          <Route path="admin/coupons" element={<Coupon />} />
-          <Route path="admin/sub-categories" element={<SubCategoryPage />} />
-          <Route path="admin/products" element={<Products />} />
-          <Route path="admin/products/new" element={<NewProduct />} />
-          <Route path="admin/products/view/:_id" element={<ProductView />} />
-          <Route path="admin/products/edit/:_id" element={<EditProduct />} />
-
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="coupons" element={<Coupon />} />
+          <Route path="sub-categories" element={<SubCategoryPage />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/new" element={<NewProduct />} />
+          <Route path="products/view/:_id" element={<ProductView />} />
+          <Route path="products/edit/:_id" element={<EditProduct />} />
           <Route
-            path="admin/products/products-dashboard"
+            path="products/products-dashboard"
             element={<ProductsDashboard />}
           />
-          <Route path="admin/users" element={<User />} />
-          <Route path="admin/orders" element={<Orders />} />
-          <Route path="admin/reviews" element={<Reviews />} />
-          <Route path="admin/admins" element={<Admin />} />
-          <Route path="admin/profile" element={<Profile />} />
+          <Route path="users" element={<User />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="admins" element={<Admin />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
 
-        <Route path="*" element={<h1>404 Page not found!</h1>} />
+        {/* Catch-All Route */}
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
       </Routes>
       <ToastContainer />
     </div>
