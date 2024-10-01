90d75c90025dfbc21c8d44dc82e60e7a59a1360b
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-10-01 10:45:54 +1000

    footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,23 +2,11 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-info text-light py-4">
-      <div className="container text-center">
-        <h5 className="mb-3">Admin CMS for RS Gifting Store</h5>
-        <p className="mb-0">Admin v3.6</p>
-        <div className="mt-3">
-          <a href="#" className="text-light mx-2">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="#" className="text-light mx-2">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="#" className="text-light mx-2">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="#" className="text-light mx-2">
-            <i className="fab fa-linkedin-in"></i>
-          </a>
+    <footer className="bg-info text-light py-3">
+      <div className="container position-relative">
+        <div className="curve"></div>
+        <div className="text-center">
+          Admin CMS for RS Gifting store. Admin v3.6
         </div>
       </div>
     </footer>
